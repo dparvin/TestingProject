2be674094d920e8d01b995ddc71f46c808593ab6
David Parvin <dwparvin@cableone.net>
2018-04-27 19:15:48 -0600

    Added Tests

---
+++ TestingProject/TestingProject/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForTrue()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestForFalse()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
